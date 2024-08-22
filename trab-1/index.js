3462cba3b3a61e2b3b1bad21f5e9891e3cecbf7c
Asafe Cruz <asafe5151@gmail.com>
2024-08-21 21:46:35 -0300

    primeira entrega

---
+++ trab-1/index.js
@@ -0,0 +1,11 @@
+const nome = prompt("Insira o seu nome")
+
+const saida = nome + "" + "inserido"
+
+   alert(saida)
+
+
+
+   const numero1 = Number(prompt("Digite um número"))
+   const numero2 = Number(prompt("Digite outro número"))
+   alert ( numero1 + numero2)
